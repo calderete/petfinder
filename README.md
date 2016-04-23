3a44e228aa081cc4225280c6b3747398d6a644d8
Charles <codprestige10@gmail.com>
2016-04-23 15:57:40 -0400

    broke up into classes and made able to take some user input. Also added ability to create files instead of using only one file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ##PetFinder
 
-This is my first simple web scraper, currently it pulls all pet listings in atlanta off of craigslist and saves them into a csv file.
+This is my first simple web scraper, currently it pulls all pet listings in the city that you choose off of craigslist and saves them into a csv file.
--- webboss_1.rb
+++ webboss_1.rb
@@ -4,19 +4,48 @@ require "HTTParty"
 require "csv"
 require "JSON"
 
-page = HTTParty.get('https://atlanta.craigslist.org/search/pet?s=0')
+class GetListings
 
-parse_page = Nokogiri::HTML(page)
+	def initialize
+		puts "Which city do you wish to search?"
+		city = gets.chomp.downcase
+		get_listings(city)
+	end
 
-pets_array = []
-
-parse_page.css('.content').css('.row').css('.hdrlnk').map do |a|
-	post_name = a.text
-	pets_array.push(post_name)
+	def get_listings(city)
+		page = HTTParty.get("https://#{city}.craigslist.org/search/pet?s=0")
+		parse_page = Nokogiri::HTML(page)
+		pets_array = []
+		parse_page.css('.content').css('.row').css('.hdrlnk').map do |a|
+			post_name = a.text
+			pets_array.push(post_name)
+		end
+		 CreateFile.new.push_to_file(pets_array)
+	end
 end
 
-CSV.open('pets.csv', 'w') do |csv|
-	csv << pets_array
+class CreateFile
+	def push_to_file(pets_array)
+		puts "What would you like to name this search?"
+		name = gets.chomp
+		date = DateTime.now
+		CSV.new("pet_search_#{name}_#{date}.csv")
+		CSV.open("pet_search_#{name}_#{date}.csv", "w") do |csv|
+			csv << pets_array
+		end
+	end
 end
 
+run = GetListings.new
+run
 #Pry.start(binding)
+
+
+
+
+
+
+
+
+
+
